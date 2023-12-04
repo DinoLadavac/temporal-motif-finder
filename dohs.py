07156a002cbdcfe3888216c442f776d8a8918844
Dino <ladavac.dino@gmail.com>
2023-12-04 20:36:59 +0100

    Updated and fixed

--- dohs.py
+++ dohs.py
@@ -4,72 +4,72 @@ import matplotlib.pyplot as plt
 def ReverseListElement(edge):
     return [edge[1], edge[0], edge[2]]
 
-def SameSourceSearch(first_edge, second_edge, matrix, timestamp_start, timestap_stop, visited):
+def SameSourceSearch(first_edge, second_edge, matrix, delta, visited):
     for edges in matrix:
         motif = [first_edge,second_edge,edges]
         motif_sorted = sorted(motif, key=lambda x: x[-1])
         to_check = tuple(motif_sorted)
+        triple = [first_edge, second_edge, edges]
+        sorted_triple = sorted(triple, key = lambda x: x[-1])
+        reversed_triplet=ReverseListElement(sorted_triple[2])
         #to_check = {frozenset(inner_list) for inner_list in motif}
         reversed_edge = ReverseListElement(edges)
-        if (edges[2]>=timestamp_start and edges[2]<timestap_stop) and (to_check not in visited) and (edges!=first_edge and edges!=second_edge): #and (((first_edge[1] == edges[0] and second_edge[1] == edges[1]) or (first_edge[0:2] == second_edge[0:2] and first_edge[0:2]==edges[0:2])) or (first_edge[0:2] == second_edge[0:2] and second_edge[0:2]==reversed_edge[0:2]) or (first_edge[0:2]==second_edge[0:2] and first_edge[0]==edges[0]) or (first_edge[0:2]==second_edge[0:2] and first_edge[0]==edges[1]) or (first_edge[0:2]==second_edge[0:2] and first_edge[0]==edges[0])):
-                    #print("Motifs: ", first_edge, second_edge, edges, visited)
-                    #print("to_check: ", to_check, "visited:", visited)
-                    visited.append(to_check)
-                    triple = [first_edge, second_edge, edges]
-                    sorted_triple = sorted(triple, key = lambda x: x[-1])
-                    reversed_triplet=ReverseListElement(sorted_triple[2])
-                    #print("Sorted Edges in Motif: ", sorted_triple)
-                    if sorted_triple[0][0] == sorted_triple[2][0] and sorted_triple[1][0] == sorted_triple[2][1] and sorted_triple[1][1] == sorted_triple[0][1]:
-                        print("M13:", sorted_triple)
-                        M13.append(sorted_triple)
-                    elif sorted_triple[0][0] == sorted_triple[2][1] and sorted_triple[1][0] == sorted_triple[2][0] and sorted_triple[0][1] == sorted_triple[1][1]:
-                        print("M14:", sorted_triple)
-                        M14.append(sorted_triple)
-                    elif sorted_triple[0][0] == sorted_triple[2][0] and sorted_triple[1][1] == sorted_triple[2][1] and sorted_triple[1][0] == sorted_triple[0][1] :
-                        print("M23:", sorted_triple)
-                        M23.append(sorted_triple)  
-                    elif sorted_triple[0][1] == sorted_triple[2][1] and sorted_triple[1][0] == sorted_triple[2][0] and sorted_triple[1][1] == sorted_triple[0][0]:
-                        print("M36:" ,sorted_triple)
-                        M36.append(sorted_triple)
-                    elif sorted_triple[0][1] == sorted_triple[2][0] and sorted_triple[1][1] == sorted_triple[2][1] and sorted_triple[0][0] == sorted_triple[1][0]:
-                        print("M45:", sorted_triple)
-                        M45.append(sorted_triple)
-                    elif sorted_triple[0][1] == sorted_triple[2][1] and sorted_triple[1][1] == sorted_triple[2][0] and sorted_triple[0][0] == sorted_matrix[1][0]:
-                        print("M46: ", sorted_triple)
-                        M46.append(sorted_triple)
-                    elif sorted_triple[0][0:2] == sorted_triple[1][0:2] and sorted_triple[1][0:2]==sorted_triple[2][0:2]:
-                        print("M61: ",sorted_triple)
-                        M61.append(sorted_triple)
-                    elif sorted_triple[0][0:2] == sorted_triple[1][0:2] and sorted_triple[1][0:2]==reversed_triplet[0:2]:
-                        print("M62:",sorted_triple)
-                        M62.append(sorted_triple)
+        if (sorted_triple[2][2] - sorted_triple[0][2] <= delta) and (to_check not in visited) and (edges!=first_edge and edges!=second_edge):
+            #print("Motifs: ", first_edge, second_edge, edges, visited)
+            #print("to_check: ", to_check, "visited:", visited)
+            visited.append(to_check)
+            #print("Sorted Edges in Motif: ", sorted_triple)
+            if sorted_triple[0][0] == sorted_triple[2][0] and sorted_triple[1][0] == sorted_triple[2][1] and sorted_triple[1][1] == sorted_triple[0][1]:
+                #print("M13:", sorted_triple)
+                M13.append(sorted_triple)
+            elif sorted_triple[0][0] == sorted_triple[2][1] and sorted_triple[1][0] == sorted_triple[2][0] and sorted_triple[0][1] == sorted_triple[1][1]:
+                #print("M14:", sorted_triple)
+                M14.append(sorted_triple)
+            elif sorted_triple[0][0] == sorted_triple[2][0] and sorted_triple[1][1] == sorted_triple[2][1] and sorted_triple[1][0] == sorted_triple[0][1] :
+                #print("M23:", sorted_triple)
+                M23.append(sorted_triple)  
+            elif sorted_triple[0][1] == sorted_triple[2][1] and sorted_triple[1][0] == sorted_triple[2][0] and sorted_triple[1][1] == sorted_triple[0][0]:
+                #print("M36:" ,sorted_triple)
+                M36.append(sorted_triple)
+            elif sorted_triple[0][1] == sorted_triple[2][0] and sorted_triple[1][1] == sorted_triple[2][1] and sorted_triple[0][0] == sorted_triple[1][0]:
+                #print("M45:", sorted_triple)
+                M45.append(sorted_triple)
+            elif sorted_triple[0][1] == sorted_triple[2][1] and sorted_triple[1][1] == sorted_triple[2][0] and sorted_triple[0][0] == sorted_triple[1][0]:
+                #print("M46: ", sorted_triple)
+                M46.append(sorted_triple)
+            elif sorted_triple[0][0:2] == sorted_triple[1][0:2] and sorted_triple[1][0:2]==sorted_triple[2][0:2]:
+                #print("M61: ",sorted_triple)
+                M61.append(sorted_triple)
+            elif sorted_triple[0][0:2] == sorted_triple[1][0:2] and sorted_triple[1][0:2]==reversed_triplet[0:2]:
+                #print("M62:",sorted_triple)
+                M62.append(sorted_triple)
 
                     
-def SourceFirstSearch(first_edge, second_edge, matrix, timestamp_start, timestap_stop, visited):
+def SourceFirstSearch(first_edge, second_edge, matrix, delta, visited):
     for edges in matrix:
         motif = [first_edge,second_edge,edges]
         motif_sorted = sorted(motif, key=lambda x: x[-1])
         sorted_triple = sorted(motif, key = lambda x: x[-1])
         to_check = tuple(motif_sorted)
         #to_check = {frozenset(inner_list) for inner_list in motif}
-        if (edges[2]>=timestamp_start and edges[2]<timestap_stop) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][1]==sorted_triple[2][0] and sorted_triple[1][0]==sorted_triple[2][1]) and sorted_triple[1][1] == sorted_triple[0][0]:      
+        if (sorted_triple[2][2] - sorted_triple[0][2] <= delta) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][1]==sorted_triple[2][0] and sorted_triple[1][0]==sorted_triple[2][1]) and sorted_triple[1][1] == sorted_triple[0][0]:      
             #print(motif)
             visited.append(to_check)
-            print("M35:", motif)
+            #print("M35:", motif)
             M35.append(motif)
-        elif (edges[2]>=timestamp_start and edges[2]<timestap_stop) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][1]==sorted_triple[1][0] and sorted_triple[1][1]==sorted_triple[2][0]) and sorted_triple[2][0:2] == sorted_triple[0][0:2]:      
+        elif (sorted_triple[2][2] - sorted_triple[0][2] <= delta) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][1]==sorted_triple[1][0] and sorted_triple[1][1]==sorted_triple[2][0]) and sorted_triple[2][0:2] == sorted_triple[0][0:2]:      
             #print(motif)
             visited.append(to_check)
-            print("M51:", motif)
+            #print("M51:", motif)
             M51.append(motif)
-        elif (edges[2]>=timestamp_start and edges[2]<timestap_stop) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][1]==sorted_triple[1][0] and sorted_triple[1][1]==sorted_triple[0][0]) and sorted_triple[2][0:2] == sorted_triple[1][0:2]:      
+        elif (sorted_triple[2][2] - sorted_triple[0][2] <= delta) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][1]==sorted_triple[1][0] and sorted_triple[1][1]==sorted_triple[0][0]) and sorted_triple[2][0:2] == sorted_triple[1][0:2]:      
             #print(motif)
             visited.append(to_check)
-            print("M52:", motif)
+            #print("M52:", motif)
             M52.append(motif)
 
             
-def TargetFirstSearch(first_edge, second_edge, matrix, timestamp_start, timestap_stop, visited):
+def TargetFirstSearch(first_edge, second_edge, matrix, delta, visited):
     for edges in matrix:
         motif = [first_edge,second_edge,edges]
         motif_sorted = sorted(motif, key=lambda x: x[-1])
@@ -77,10 +77,10 @@ def TargetFirstSearch(first_edge, second_edge, matrix, timestamp_start, timestap
         to_check = tuple(motif_sorted)
         #to_check = {frozenset(inner_list) for inner_list in motif
         #print(motif,to_check)
-        if (edges[2]>=timestamp_start and edges[2]<timestap_stop) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][0]==sorted_triple[2][1] and sorted_triple[1][1]==sorted_triple[2][0] and sorted_triple[0][1] == sorted_triple[1][0]):      
+        if (sorted_triple[2][2] - sorted_triple[0][2] <= delta) and (edges!=first_edge and edges!=second_edge) and (to_check not in visited) and (sorted_triple[0][0]==sorted_triple[2][1] and sorted_triple[1][1]==sorted_triple[2][0] and sorted_triple[0][1] == sorted_triple[1][0]):      
             #print(motif)
             visited.append(to_check)
-            print("M24:", motif)
+            #print("M24:", motif)
             M24.append(motif)
                    
             
@@ -103,39 +103,39 @@ for line in lines:
     if len(parts) == 3:
         net.add_edge(int(parts[0]), int(parts[1]), weight=float(parts[2]))
 
-plt.figure(figsize=(10, 10))  
+# plt.figure(figsize=(10, 10))  
 
-pos = nx.spring_layout(net, k=0.2)  
+# pos = nx.spring_layout(net, k=0.2)  
 
-labels = {node: str(node) for node in net.nodes()}
-edge_labels = {(i, j): f"{d['weight']:.2f}" for i, j, d in net.edges(data=True)} 
+# labels = {node: str(node) for node in net.nodes()}
+# edge_labels = {(i, j): f"{d['weight']:.2f}" for i, j, d in net.edges(data=True)} 
 
-nx.draw_networkx(net, pos, with_labels=True, labels=labels, font_color="black", node_color="white", width=1.5, node_size=200, font_weight='bold')
-nx.draw_networkx_edge_labels(net, pos, edge_labels=edge_labels, font_size=10)
+# nx.draw_networkx(net, pos, with_labels=True, labels=labels, font_color="black", node_color="white", width=1.5, node_size=200, font_weight='bold')
+# nx.draw_networkx_edge_labels(net, pos, edge_labels=edge_labels, font_size=10)
 
-plt.axis('off')  
-#plt.show()
+# plt.axis('off')  
+# #plt.show()
 
 
 sorted_matrix = sorted(matx, key=lambda x: x[0])
 #print(sorted_matrix)
 
-start = float(input("Timestamp starting time (seconds): "))
-stop = float(input("Timestamp end time (seconds): "))
+# start = float(input("Timestamp starting time (seconds): "))
+# stop = float(input("Timestamp end time (seconds): "))
 
+delta = float(input("Delta (seconds): "))
 for i in matx:
-    if i[2]>= start and i[2]<stop:
-        for j in matx:
-            if j[2]>= start and j[2]<stop and i!=j:
-                #Same source
-                if i[0] == j[0]:
-                    SameSourceSearch(i,j,matx,start,stop,visited)
-                #Source of the first edge is the target of the second edge (M35)
-                elif i[0]==j[1]:
-                    SourceFirstSearch(i,j,matx,start,stop,visited)
-                #Target of the first edge is the source of the second edge (M24)
-                elif i[1]==j[0]:
-                    TargetFirstSearch(i,j,matx,start,stop,visited)
+    for j in matx:
+        if i!=j:
+            #Same source
+            if i[0] == j[0]:
+                SameSourceSearch(i,j,matx,delta,visited)
+            #Source of the first edge is the target of the second edge (M35)
+            elif i[0]==j[1]:
+                SourceFirstSearch(i,j,matx,delta,visited)
+            #Target of the first edge is the source of the second edge (M24)
+            elif i[1]==j[0]:
+                TargetFirstSearch(i,j,matx,delta,visited)
 
 print("M13: {}\nM14: {}\nM23: {}\nM24: {}\nM35: {}\nM36: {}\nM45: {}\nM46: {}\nM51: {}\nM52: {}\nM61: {}\nM62: {}".format(len(M13),len(M14),len(M23),len(M24),len(M35),len(M36),len(M45),len(M46),len(M51),len(M52),len(M61),len(M62)))
 
